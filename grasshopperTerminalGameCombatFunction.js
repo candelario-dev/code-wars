fd3fbf03fec251ef75e0deecc0c9cf20ad4ae8de
candelario-dev <rebecca.candelario.dev@gmail.com>
2020-09-04 22:57:17 -0400

    Daily challenge

---
+++ grasshopperTerminalGameCombatFunction.js
@@ -0,0 +1,12 @@
+/*Create a combat function that takes the player's current health and the
+amount of damage received, and returns the player's new health. Health can't
+be less than 0.*/
+
+function combat(health, damage) {
+  let newHealth = health - damage
+  if(newHealth > 0){
+   return newHealth
+ }else {
+   return 0
+  }
+}
